ed6e23356a696e948b7981718cba8a22d64fdc5f
Curt Lynch <curjlync@ut.utm.edu>
2023-02-14 12:26:52 -0600

    Widget chaos in Main

--- CW7 Decorator Pattern/Program.cs
+++ CW7 Decorator Pattern/Program.cs
@@ -10,6 +10,22 @@ namespace CW7_Decorator_Pattern
     {
         static void Main(string[] args)
         {
+            Widget widget = new TextField(40, 20);
+            Random rand = new Random();
+            int numOfDecorators = rand.Next() % 50 + 10;
+            for (int i = 0 ; i < numOfDecorators; i++)
+            {
+                if (rand.Next() % 2 == 0)
+                {
+                    widget = new BorderDecorator(widget);
+                }
+                else
+                {
+                    widget = new ScrollDecorator(widget);
+                }
+            }
+            widget.draw();
+            Console.ReadKey();
         }
     }
 }
